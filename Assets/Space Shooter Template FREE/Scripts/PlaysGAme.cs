91fdb34bc2a549e5b29d9441b763b4748b1d453d
gaby <chansaud.eremoana@gmail.com>
2018-11-27 09:44:24 -1000

    MAJ

---
+++ Assets/Space Shooter Template FREE/Scripts/PlaysGAme.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlaysGAme : MonoBehaviour {
+
+    public void LoadOnClick(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public GameObject destructionFX;
+
+    public static PlaysGAme instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    //method for damage proceccing by 'Player'
+    public void GetDamage(int damage)
+    {
+        Destruction();
+    }
+
+    //'Player's' destruction procedure
+    void Destruction()
+    {
+        Instantiate(destructionFX, transform.position, Quaternion.identity); //generating destruction visual effect and destroying the 'Player' object
+        Destroy(gameObject);
+    }
+
+
+}
